616d6ccb3214ab56a0bdf787d1c7265a00f67db7
larikovweb <larikov.web@gmail.com>
2023-10-30 12:23:17 +0300

    feat: plug card

---
+++ src/components/Plugs.tsx
@@ -0,0 +1,20 @@
+import styled from '@emotion/styled';
+
+const FullWidthCard = styled.div`
+  box-sizing: border-box;
+  width: 100%;
+  height: 10rem;
+  background-color: rgba(0, 0, 0, 0.2);
+  border: 0.0625rem solid rgba(0, 0, 0, 0.4);
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  border-radius: 0.75rem;
+  color: #fff;
+  font-size: 1.5rem;
+  padding: 2rem;
+`;
+
+export const Plugs = {
+  FullWidthCard,
+};
